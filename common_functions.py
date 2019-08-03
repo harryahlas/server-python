cc921057b34cb23c64ba07de0c43e5be8044cdc7
Harry Ahlas <42125399+harryahlas@users.noreply.github.com>
2019-08-02 20:34:32 -0700

    Create common_functions.py

---
+++ common_functions.py
@@ -0,0 +1,12 @@
+# from cmd
+# wget https://bootstrap.pypa.io/get-pip.py && python get-pip.py --user
+
+#list functions in module
+dir(os)
+
+
+#get help
+help(packagename)
+
+# Get current directory
+os.getcwd()
