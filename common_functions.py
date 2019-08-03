bc54122d9a36c0b7e87f8a9ee7e67ad668d03608
Harry Ahlas <42125399+harryahlas@users.noreply.github.com>
2019-08-02 20:36:40 -0700

    Update common_functions.py

--- common_functions.py
+++ common_functions.py
@@ -1,5 +1,11 @@
 # from cmd
+
+# Install pip
 # wget https://bootstrap.pypa.io/get-pip.py && python get-pip.py --user
+
+# install packages
+# ~/.local/bin/pip install pandas --user
+
 
 #list functions in module
 dir(os)
