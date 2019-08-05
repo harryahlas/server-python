f87cb7ccca7bd840bc2cebc7c026cf0b525b29dc
harryahlas <42125399+harryahlas@users.noreply.github.com>
2019-08-05 00:00:05 -0700

    Ip lookup

--- ip_country_update.py
+++ ip_country_update.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 # Initial table creation, maybe move to separate file
 # Create ip_country table, columns ip varchar(use my ip and usa for dummy)
-ip_country = pd.DataFrame([['70.36.251.205', None]], columns = ['IP'])
+ip_country = pd.DataFrame([['70.36.251.205']], columns = ['IP'])
 
 # Import initial_archive_table
 from sqlalchemy import create_engine
@@ -16,21 +16,27 @@ cnx = create_engine(engine_text, echo=False)
 
 df = pd.read_sql_query('SELECT * FROM initial_archive_table', engine_text)
 
-# create separate df ips that has unique ip_addresses only
+# create separate df ips that has unique ip_addresses only. Create blank country column
 ips = df[['IP']].copy().drop_duplicates()
 ips['COUNTRY'] = None
+ips = ips.reset_index() # needed because dropped duplicates are not in the index
 
-# Create blank country column
 # loop through and run code below for each, storing country
 
-i = 0
 
-ip_address = "93.158.161.124";
-download_url = 'http://ipinfo.io/' + ip_country.loc[i,'ip'] 
-page = requests.get(download_url)
-#page.content
-result = re.search('"country": "(.*)",', str(page.content))
-ip_country.loc[i,'country'] = result.group(1)[0:2]
+
+for i in range(len(ips)):
+    print(i)
+    download_url = 'http://ipinfo.io/' + ips.loc[i,'IP'] 
+    page = requests.get(download_url)
+    #page.content
+    result = re.search('"country": "(.*)",', str(page.content))
+    ips.loc[i,'COUNTRY'] = result.group(1)[0:2]
+    
+    #print df.iloc[i]['c1'], df.iloc[i]['c2']
+
+    
+#ip_address = "93.158.161.124";
 
 # left join ip initial_archive_table to ip_countries
 
