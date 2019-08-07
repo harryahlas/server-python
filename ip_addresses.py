1d4bccf730b572958d303af64424755be4011975
harryahlas <42125399+harryahlas@users.noreply.github.com>
2019-08-06 19:39:45 -0700

    Update ip_addresses.py

--- ip_addresses.py
+++ ip_addresses.py
@@ -30,26 +30,37 @@ reader.get('162.243.147.46')
 
 reader.close()
 
-#### Need to try for country and then if not get continent, if not then skip
-i = 1343
+# Find country in dictionary. if no Country then pick first name that comes up.
 for i in range(0,1782):#len(ips)):
     print(i)
     url_country = reader.get(ips.loc[i,'IP'])
-
     j = 0  
     for x in url_country:
-      if x == "country":
-          #print(x)
-          #print(j)
-          j = j + 1
+        if x == "country":          
           ips.loc[i,'COUNTRY'] = url_country[list(url_country)[j]]['names']['en']
+        j = j + 1
+
+ips.groupby('COUNTRY').count().sort_values(['IP'])
+         
+## OR ####
+
+#for i in range(0,1782):#len(ips)):
+#    print(i)
+#    url_country = reader.get(ips.loc[i,'IP'])
+#    ips.loc[i,'COUNTRY'] = url_country['country']['names']['en']
+#
+#    url_country = reader.get(ips.loc[336,'IP'])
+#    ips.loc[336,'COUNTRY'] = url_country['country']['names']['en']
+
+
+
 
 url_country[list(url_country)[0]]['names']['en']
 
 for x in url_country[list(url_country)[0]]:
   print(x)
 
-
+print(url_country['country']['names']['en'])
   
 for x in url_country[list(url_country)]:
   print(x)
