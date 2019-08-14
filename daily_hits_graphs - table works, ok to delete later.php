60fea0f89e68b2d41064f9090ef3c88e195cc89e
harryahlas <42125399+harryahlas@users.noreply.github.com>
2019-08-13 22:41:52 -0700

    js tests

--- daily_hits_graphs - table works, ok to delete later.php
+++ daily_hits_graphs - table works, ok to delete later.php
@@ -1,3 +1,5 @@
+<html>
+<body>
 <?php 
 
 $daily_hits_graphs_sql = "SELECT STR_TO_DATE(CONCAT(A.YEAR, A.MONTH, A.DAY), '%Y%b%d') AS HIT_DATE, 
@@ -16,7 +18,6 @@ $servername = "localhost";
 $username = "edemise";
 $password = "vo8x8i68vid2";
 
-/*
 $con=mysqli_connect($servername, $username, $password);
 // Check connection
 if (mysqli_connect_errno())
@@ -26,7 +27,7 @@ echo "Failed to connect to MySQL: " . mysqli_connect_error();
 
 //$result = mysqli_query($con,"select * from edemise.hits_archive");
 /*echo "hello";
-
+*/
 echo "hellook";
 $result = mysqli_query($con, $daily_hits_graphs_sql);
 
@@ -79,77 +80,10 @@ echo "</table>";
     header('Content-type: application/json');
     //output the return value of json encode using the echo function.
     echo json_encode($jsonArray);
-	
+*/	
 mysqli_close($con);
 
 ?>
 <a href="sql_images/daily_hits.png">daily_hits.png</a>
 </body>
 </html>
-*/
-
-
-
-
-
-<?php
-$servername = "localhost";
-$username = "edemise";
-$password = "vo8x8i68vid2";
-$dbname = "edemise";
-$dataPoints = array();
-//Best practice is to create a separate file for handling connection to database
-try{
-     // Creating a new connection.
-    // Replace your-hostname, your-db, your-username, your-password according to your database
-    $link = new \PDO(   'mysql:host=$servername;dbname=$dbname;charset=utf8mb4', //'mysql:host=localhost;dbname=canvasjs_db;charset=utf8mb4',
-                        $username, //'root',
-                        $password, //'',
-                        array(
-                            \PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                            \PDO::ATTR_PERSISTENT => false
-                        )
-                    );
-	
-    $handle = $link->prepare($daily_hits_graphs_sql); 
-    $handle->execute(); 
-    $result = $handle->fetchAll(\PDO::FETCH_OBJ);
-		
-    foreach($result as $row){
-        array_push($dataPoints, array("x"=> $row->x, "y"=> $row->y));
-    }
-	$link = null;
-}
-catch(\PDOException $ex){
-    print($ex->getMessage());
-}
-	
-?>
-<!DOCTYPE HTML>
-<html>
-<head>  
-<script>
-window.onload = function () {
- 
-var chart = new CanvasJS.Chart("chartContainer", {
-	animationEnabled: true,
-	exportEnabled: true,
-	theme: "light1", // "light1", "light2", "dark1", "dark2"
-	title:{
-		text: "PHP Column Chart from Database"
-	},
-	data: [{
-		type: "column", //change type to bar, line, area, pie, etc  
-		dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
-	}]
-});
-chart.render();
- 
-}
-</script>
-</head>
-<body>
-<div id="chartContainer" style="height: 370px; width: 100%;"></div>
-<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
-</body>
-</html>       
